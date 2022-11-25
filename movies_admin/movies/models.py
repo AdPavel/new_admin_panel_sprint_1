84f4cca43a30f663bf4e4b51f135e36513464ec1
PavelAd <adap2@mail.ru>
2022-11-26 01:00:48 +0400

    Изменил длину полей на 50 full_name в Person и role в PersonFilmwork

--- movies_admin/movies/models.py
+++ movies_admin/movies/models.py
@@ -1,7 +1,11 @@
 import uuid
+
 from django.db import models
+
 from django.core.validators import MinValueValidator, MaxValueValidator
+
 from django.utils.translation import gettext_lazy as _
+
 
 class TimeStampedMixin(models.Model):
     # auto_now_add автоматически выставит дату создания записи.
@@ -17,7 +21,8 @@ class TimeStampedMixin(models.Model):
 class UUIDMixin(models.Model):
     # Типичная модель в Django использует число в качестве id. В таких ситуациях поле не описывается в модели.
     # Вам же придётся явно объявить primary key.
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          editable=False)
 
     class Meta:
         abstract = True
@@ -41,7 +46,7 @@ class Genre(UUIDMixin, TimeStampedMixin):
 
 
 class Person(UUIDMixin, TimeStampedMixin):
-    full_name = models.CharField(_('Full name'), max_length=30)
+    full_name = models.CharField(_('Full name'), max_length=50)
 
     class Meta:
         db_table = "content\".\"person"
@@ -56,20 +61,15 @@ class Filmwork(UUIDMixin, TimeStampedMixin):
     title = models.CharField(_('title'), max_length=255)
     description = models.TextField(_('description'), blank=True)
     creation_date = models.DateField(_('create date'))
-    rating = models.FloatField(_('rating'), blank=True,
-                               validators=[MinValueValidator(0),
-                                           MaxValueValidator(100)])
+    rating = models.FloatField(_('rating'), blank=True, validators=[MinValueValidator(0), MaxValueValidator(100)])
     type = models.CharField(_('type'), max_length=255,
-                            choices=[('movie', _('type movie')),
-                                     ('tv_show', _('type TV show'))])
+                            choices=[('movie', _('type movie')), ('tv_show', _('type TV show'))])
     genres = models.ManyToManyField(Genre, through='GenreFilmwork')
     persons = models.ManyToManyField(Person, through='PersonFilmwork')
-    certificate = models.CharField(_('certificate'), max_length=512,
-                                   blank=True)
+    certificate = models.CharField(_('certificate'), max_length=512, blank=True)
     # Параметр upload_to указывает, в какой подпапке будут храниться загружемые файлы.
     # Базовая папка указана в файле настроек как MEDIA_ROOT
-    file_path = models.FileField(_('file'), blank=True,
-                                 null=True, upload_to='movies/')
+    file_path = models.FileField(_('file'), blank=True, null=True, upload_to='movies/')
 
     class Meta:
         db_table = "content\".\"film_work"
@@ -97,7 +97,7 @@ class GenreFilmwork(UUIDMixin):
 class PersonFilmwork(UUIDMixin):
     film_work = models.ForeignKey('Filmwork', on_delete=models.CASCADE)
     person = models.ForeignKey('Person', on_delete=models.CASCADE)
-    role = models.CharField(_('role'), max_length=30, null=True)
+    role = models.CharField(_('role'), max_length=50, null=True)
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
@@ -107,5 +107,3 @@ class PersonFilmwork(UUIDMixin):
 
     def __str__(self):
         return self.role
-
-

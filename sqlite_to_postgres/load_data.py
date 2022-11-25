8351bcd5a00c5f99bbfd1b901a9d32f45069f102
PavelAd <adap2@mail.ru>
2022-11-26 01:01:49 +0400

    Добавил контекст менеджера для скуля

--- sqlite_to_postgres/load_data.py
+++ sqlite_to_postgres/load_data.py
@@ -1,20 +1,28 @@
 import sqlite3
-
 import psycopg2
+import os
+
+from contextlib import contextmanager
 from psycopg2.extensions import connection as _connection
 from psycopg2.extras import DictCursor
-
-import os
 from dotenv import load_dotenv
-
-from .extractor_saver import SQLiteExtractor, PostgresSaver
+from extractor_saver import SQLiteExtractor, PostgresSaver
 
 load_dotenv()
 DB_NAME = os.environ.get('DB_NAME')
 DB_USER = os.environ.get('DB_USER')
 DB_PASSWORD = os.environ.get('DB_PASSWORD')
+DB_PATH = os.environ.get('DB_PATH')
 HOST = os.environ.get('HOST')
 PORT = os.environ.get('PORT')
+
+
+@contextmanager
+def conn_context_sqlite(db_path: str):
+    conn = sqlite3.connect(db_path)
+    conn.row_factory = sqlite3.Row
+    yield conn
+    conn.close()
 
 
 def load_from_sqlite(connection: sqlite3.Connection, pg_conn: _connection):
@@ -25,7 +33,8 @@ def load_from_sqlite(connection: sqlite3.Connection, pg_conn: _connection):
     data = sqlite_extractor.extract_movies()
     postgres_saver.save_all_data(data)
 
+
 if __name__ == '__main__':
     dsl = {'dbname': DB_NAME, 'user': DB_USER, 'password': DB_PASSWORD, 'host': HOST, 'port': PORT}
-    with sqlite3.connect('db.sqlite') as sqlite_conn, psycopg2.connect(**dsl, cursor_factory=DictCursor) as pg_conn:
+    with conn_context_sqlite(DB_PATH) as sqlite_conn, psycopg2.connect(**dsl, cursor_factory=DictCursor) as pg_conn:
         load_from_sqlite(sqlite_conn, pg_conn)

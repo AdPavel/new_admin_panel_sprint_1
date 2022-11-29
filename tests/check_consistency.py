d4a942fde412c995d6a82bfb8d043316df0172ef
PavelAd <adap2@mail.ru>
2022-11-30 00:28:08 +0400

    Правки по flake8

--- tests/check_consistency.py
+++ tests/check_consistency.py
@@ -7,7 +7,6 @@ import maya
 from psycopg2.extras import DictCursor
 from dotenv import load_dotenv
 
-
 load_dotenv()
 DB_NAME = os.environ.get('DB_NAME')
 DB_USER = os.environ.get('DB_USER')
@@ -19,8 +18,10 @@ PORT = os.environ.get('PORT')
 
 @pytest.fixture()
 def connection():
-    dsl = {'dbname': DB_NAME, 'user': DB_USER, 'password': DB_PASSWORD, 'host': HOST, 'port': PORT}
-    with sqlite3.connect(DB_PATH) as sqlite_conn, psycopg2.connect(**dsl, cursor_factory=DictCursor) as pg_conn:
+    dsl = {'dbname': DB_NAME, 'user': DB_USER, 'password': DB_PASSWORD,
+           'host': HOST, 'port': PORT}
+    with sqlite3.connect(DB_PATH) as sqlite_conn, \
+            psycopg2.connect(**dsl, cursor_factory=DictCursor) as pg_conn:
         return sqlite_conn.cursor(), pg_conn.cursor()
 
 
@@ -31,7 +32,10 @@ def test_table_exist_pg(connection):
     sql_curs.execute(sql_query)
     sql_tables = [value[0] for value in sql_curs.fetchall()]
 
-    pg_query = r"""SELECT tablename FROM pg_tables WHERE schemaname='content';"""
+    pg_query = r"""
+                SELECT tablename
+                FROM pg_tables WHERE schemaname='content';
+                """
     pg_curs.execute(pg_query)
     for table in pg_curs.fetchall():
         assert table[0] in sql_tables, f'В PG нет таблицы {table[0]}'
@@ -39,7 +43,8 @@ def test_table_exist_pg(connection):
 
 def test_count_record(connection):
     sql_curs, pg_curs = connection
-    tables = ('film_work', 'person', 'genre', 'genre_film_work', 'person_film_work')
+    tables = ('film_work', 'person', 'genre',
+              'genre_film_work', 'person_film_work')
     for table_name in tables:
         sql_query = f"""SELECT count(*) FROM {table_name};"""
         sql_curs.execute(sql_query)
@@ -47,20 +52,22 @@ def test_count_record(connection):
         pg_query = f"""SELECT count(*) FROM content.{table_name};"""
         pg_curs.execute(pg_query)
         count_pg_table = pg_curs.fetchone()
-        assert count_sql_table[0] == count_pg_table[0], f'Количество записей для {table_name} не совпадает'
+        assert count_sql_table[0] == count_pg_table[0], \
+            f'Количество записей для {table_name} не совпадает'
 
 
 def test_rows_filmworks(connection):
     sql_curs, pg_curs = connection
-    sql_query = f"""
-        SELECT id, title, description, creation_date, rating, type, file_path, created_at, updated_at
-        FROM film_work;
-        """
+    sql_query = r"""
+        SELECT id, title, description, creation_date,
+                rating, type, file_path, created_at, updated_at
+        FROM film_work;"""
     sql_curs.execute(sql_query)
     sql_table = sql_curs.fetchall()
 
-    pg_query = f"""
-        SELECT id, title, description, creation_date, rating, type, file_path, created, modified
+    pg_query = r"""
+        SELECT id, title, description, creation_date,
+                rating, type, file_path, created, modified
         FROM content.film_work;"""
     pg_curs.execute(pg_query)
 
@@ -68,23 +75,25 @@ def test_rows_filmworks(connection):
     for i in range(len(sql_table)):
         for idx in range(9):
             if idx in (7, 8):
-                assert maya.parse(sql_table[i][idx]).datetime() == pg_table[i][idx],\
-                    f'Не соврадение полей {sql_table[i][idx]} и {pg_table[i][idx]}'
+                assert maya.parse(sql_table[i][idx]).datetime() == pg_table[i][idx], \
+                    f"""Не совпадают поля {sql_table[i][idx]}
+                    и {pg_table[i][idx]}"""
                 continue
-            assert sql_table[i][idx] == pg_table[i][idx],\
-                f'Не соврадение полей {sql_table[i][idx]} и {pg_table[i][idx]}'
+            assert sql_table[i][idx] == pg_table[i][idx], \
+                f"""Не совпадают поля {sql_table[i][idx]}
+                и {pg_table[i][idx]}"""
 
 
 def test_rows_person(connection):
     sql_curs, pg_curs = connection
-    sql_query = f"""
+    sql_query = r"""
         SELECT id, full_name, created_at, updated_at
         FROM person;
         """
     sql_curs.execute(sql_query)
     sql_table = sql_curs.fetchall()
 
-    pg_query = f"""
+    pg_query = r"""
         SELECT id, full_name, created, modified
         FROM content.person;"""
     pg_curs.execute(pg_query)
@@ -93,22 +102,25 @@ def test_rows_person(connection):
     for i in range(len(sql_table)):
         for idx in range(4):
             if idx in (2, 3):
-                assert maya.parse(sql_table[i][idx]).datetime() == pg_table[i][idx],\
-                    f'Не соврадение полей {sql_table[i][idx]} и {pg_table[i][idx]}'
+                assert maya.parse(sql_table[i][idx]).datetime() == pg_table[i][idx], \
+                    f"""Не совпадают поля {sql_table[i][idx]}
+                    и {pg_table[i][idx]}"""
                 continue
-            assert sql_table[i][idx] == pg_table[i][idx],\
-                f'Не соврадение полей {sql_table[i][idx]} и {pg_table[i][idx]}'
+            assert sql_table[i][idx] == pg_table[i][idx], \
+                f"""Не совпадают поля {sql_table[i][idx]}
+                и {pg_table[i][idx]}"""
+
 
 def test_rows_genre(connection):
     sql_curs, pg_curs = connection
-    sql_query = f"""
+    sql_query = r"""
         SELECT id, name, description, created_at, updated_at
         FROM genre;
         """
     sql_curs.execute(sql_query)
     sql_table = sql_curs.fetchall()
 
-    pg_query = f"""
+    pg_query = r"""
         SELECT id, name, description, created, modified
         FROM content.genre;"""
     pg_curs.execute(pg_query)
@@ -117,23 +129,25 @@ def test_rows_genre(connection):
     for i in range(len(sql_table)):
         for idx in range(5):
             if idx in (3, 4):
-                assert maya.parse(sql_table[i][idx]).datetime() == pg_table[i][idx],\
-                    f'Не соврадение полей {sql_table[i][idx]} и {pg_table[i][idx]}'
+                assert maya.parse(sql_table[i][idx]).datetime() == pg_table[i][idx], \
+                    f"""Не совпадают поля {sql_table[i][idx]}
+                    и {pg_table[i][idx]}"""
                 continue
-            assert sql_table[i][idx] == pg_table[i][idx],\
-                f'Не соврадение полей {sql_table[i][idx]} и {pg_table[i][idx]}'
+            assert sql_table[i][idx] == pg_table[i][idx], \
+                f"""Не совпадают поля {sql_table[i][idx]}
+                    и {pg_table[i][idx]}"""
 
 
 def test_rows_genre_film_work(connection):
     sql_curs, pg_curs = connection
-    sql_query = f"""
+    sql_query = r"""
         SELECT id, film_work_id, genre_id, created_at
         FROM genre_film_work;
         """
     sql_curs.execute(sql_query)
     sql_table = sql_curs.fetchall()
 
-    pg_query = f"""
+    pg_query = r"""
         SELECT id, film_work_id, genre_id, created
         FROM content.genre_film_work;"""
     pg_curs.execute(pg_query)
@@ -142,23 +156,25 @@ def test_rows_genre_film_work(connection):
     for i in range(len(sql_table)):
         for idx in range(4):
             if idx == 3:
-                assert maya.parse(sql_table[i][idx]).datetime() == pg_table[i][idx],\
-                    f'Не соврадение полей {sql_table[i][idx]} и {pg_table[i][idx]}'
+                assert maya.parse(sql_table[i][idx]).datetime() == pg_table[i][idx], \
+                    f"""Не совпадают поля {sql_table[i][idx]}
+                    и {pg_table[i][idx]}"""
                 continue
-            assert sql_table[i][idx] == pg_table[i][idx],\
-                f'Не соврадение полей {sql_table[i][idx]} и {pg_table[i][idx]}'
+            assert sql_table[i][idx] == pg_table[i][idx], \
+                f"""Не совпадают поля {sql_table[i][idx]}
+                и {pg_table[i][idx]}"""
 
 
 def test_rows_person_film_work(connection):
     sql_curs, pg_curs = connection
-    sql_query = f"""
+    sql_query = r"""
         SELECT id, film_work_id, person_id, role, created_at
         FROM person_film_work;
         """
     sql_curs.execute(sql_query)
     sql_table = sql_curs.fetchall()
 
-    pg_query = f"""
+    pg_query = r"""
         SELECT id, film_work_id, person_id, role, created
         FROM content.person_film_work;"""
     pg_curs.execute(pg_query)
@@ -167,14 +183,10 @@ def test_rows_person_film_work(connection):
     for i in range(len(sql_table)):
         for idx in range(5):
             if idx == 4:
-                assert maya.parse(sql_table[i][idx]).datetime() == pg_table[i][idx],\
-                    f'Не соврадение полей {sql_table[i][idx]} и {pg_table[i][idx]}'
+                assert maya.parse(sql_table[i][idx]).datetime() == pg_table[i][idx], \
+                    f"""Не совпадают поля {sql_table[i][idx]}
+                        и {pg_table[i][idx]}"""
                 continue
-            assert sql_table[i][idx] == pg_table[i][idx],\
-                f'Не соврадение полей {sql_table[i][idx]} и {pg_table[i][idx]}'
-
-
-
-
-
-
+            assert sql_table[i][idx] == pg_table[i][idx], \
+                f"""Не совпадают поля {sql_table[i][idx]}
+                    и {pg_table[i][idx]}"""
